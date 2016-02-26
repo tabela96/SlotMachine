389fc9ff510ac133e317d29ea0355c6ea1e0a624
samuelegastaldo <samuelegastaldo@Lab06_19.itseinaudi.local>
2016-02-26 13:11:17 +0100

    Fatta bene

--- SlotMachine/src/gastaldodemian/ImagesContainer.java
+++ SlotMachine/src/gastaldodemian/ImagesContainer.java
@@ -3,11 +3,22 @@ package gastaldodemian;
 import javax.swing.ImageIcon;
 
 public class ImagesContainer {
-	String[] images = new String[8];
+	private String[] images = new String[8];
+	
 	
 	public ImagesContainer(){
-		images[0] = "";
-		
+		images[0] = "/Immagini/anguria.PNG";
+		images[1] = "/Immagini/arancia.PNG";
+		images[2] = "/Immagini/banana.PNG";
+		images[3] = "/Immagini/bar.PNG";
+		images[4] = "/Immagini/ciliegia.PNG";
+		images[5] = "/Immagini/limone.PNG";
+		images[6] = "/Immagini/mela.PNG";
+		images[7] = "/Immagini/uva.PNG";
+	}
+
+	public String getImage(int i){
+		return images[i];
 	}
 	
 }
--- SlotMachine/src/gastaldodemian/SlotMachine.java
+++ SlotMachine/src/gastaldodemian/SlotMachine.java
@@ -4,6 +4,8 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Label;
 
+import java.util.Random;
+
 import javax.swing.ImageIcon;
 
 import org.eclipse.swt.SWT;
@@ -13,11 +15,18 @@ import com.sun.prism.Image;
 
 import org.eclipse.ui.forms.widgets.FormToolkit;
 import org.eclipse.ui.forms.widgets.ImageHyperlink;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 
 public class SlotMachine {
 
 	protected Shell shlSlotMachine;
 	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
+	private Label[] slots = new Label[3];
+	private ImagesContainer immagini = new ImagesContainer();
+	private int[] numeri = new int[3];
+	private Label titolo;
 
 	/**
 	 * Launch the application.
@@ -52,7 +61,49 @@ public class SlotMachine {
 	 */
 	protected void createContents() {
 		shlSlotMachine = new Shell();
-		shlSlotMachine.setSize(597, 390);
+		shlSlotMachine.setSize(600, 400);
 		shlSlotMachine.setText("Slot Machine");
+		
+		slots[0] = formToolkit.createLabel(shlSlotMachine, "New Label", SWT.NONE);
+		slots[0].setBounds(74, 100, 150, 150);
+		slots[0].setImage(SWTResourceManager.getImage(SlotMachine.class, "/Immagini/anguria.PNG"));
+		
+		slots[1] = formToolkit.createLabel(shlSlotMachine, "New Label", SWT.NONE);
+		slots[1].setBounds(225, 100, 150, 150);
+		slots[1].setImage(SWTResourceManager.getImage(SlotMachine.class, "/Immagini/uva.PNG"));
+		
+		slots[2] = formToolkit.createLabel(shlSlotMachine, "New Label", SWT.NONE);
+		slots[2].setBounds(376, 100, 150, 150);
+		slots[2].setImage(SWTResourceManager.getImage(SlotMachine.class, "/Immagini/bar.PNG"));
+		
+		Button btnGenera = new Button(shlSlotMachine, SWT.NONE);
+		btnGenera.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				generaSlot();
+			}
+		});
+		btnGenera.setBounds(10, 282, 561, 68);
+		formToolkit.adapt(btnGenera, true, true);
+		btnGenera.setText("GIOCA!");		
+		
+		titolo = new Label(shlSlotMachine, SWT.NONE);
+		titolo.setAlignment(SWT.CENTER);
+		titolo.setBounds(4, 4, 567, 68);
+		titolo.setText("Best slot machine ever made by Gastaldo && Demian Oleksandr");
+	}
+	
+	private void generaSlot(){
+		for(int i=0;i<3;i++){
+			numeri[i] = (int) (Math.random()*8);
+			slots[i].setImage(SWTResourceManager.getImage(SlotMachine.class, immagini.getImage(numeri[i])));
+		}
+		Controlla();
+	}
+	
+	private void Controlla(){
+		if(numeri[0] != numeri[1] && numeri[1] != numeri[2]){
+			titolo.setText("Hai stra perso, perdente");
+		}
 	}
 }
