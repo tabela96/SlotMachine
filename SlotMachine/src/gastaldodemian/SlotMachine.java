8ddcde2d1dd0b1530eba68ab11211d95b876547d
demianoleksandr <demianoleksandr@Lab06_21.itseinaudi.local>
2016-02-27 09:56:58 +0100

    Slot girano, ma non del tutto

--- SlotMachine/src/gastaldodemian/SlotMachine.java
+++ SlotMachine/src/gastaldodemian/SlotMachine.java
@@ -18,20 +18,42 @@ import org.eclipse.ui.forms.widgets.ImageHyperlink;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Composite;
+
 
 public class SlotMachine {
+	
+	public class GiraSlot extends Thread {
+		
+		public void run() {
+			int t = 0;
+			while(t < 303){
+				for(int i = 0; i < 3; i++){
+					slots[i].setBounds(slots[i].getBounds().x, slots[i].getBounds().y + 1, 150, 150);
+					System.out.println(i + " " + slots[i].getBounds());
+					if(slots[i].getBounds().y > 151){
+						generaSlot();
+						slots[i].setBounds(slots[i].getBounds().x, -151, 150, 150);
+					}
+				}
+				try {
+					this.sleep(1);
+					t++;
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+	    }
+	}
 
 	protected Shell shlSlotMachine;
 	private final FormToolkit formToolkit = new FormToolkit(Display.getDefault());
-	private Label[] slots = new Label[3];
+	private Label[] slots = new Label[6];
 	private ImagesContainer immagini = new ImagesContainer();
 	private int[] numeri = new int[3];
 	private Label titolo;
-
-	/**
-	 * Launch the application.
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
 		try {
 			SlotMachine window = new SlotMachine();
@@ -64,23 +86,29 @@ public class SlotMachine {
 		shlSlotMachine.setSize(600, 400);
 		shlSlotMachine.setText("Slot Machine");
 		
-		slots[0] = formToolkit.createLabel(shlSlotMachine, "New Label", SWT.NONE);
-		slots[0].setBounds(74, 100, 150, 150);
+		Composite composite = new Composite(shlSlotMachine, SWT.NONE);
+		composite.setBounds(11, 71, 560, 152);
+		formToolkit.adapt(composite);
+		formToolkit.paintBordersFor(composite);
+		
+		slots[0] = formToolkit.createLabel(composite, "New Label", SWT.NONE);
+		slots[0].setBounds(10, 1, 150, 150);
 		slots[0].setImage(SWTResourceManager.getImage(SlotMachine.class, "/Immagini/anguria.PNG"));
 		
-		slots[1] = formToolkit.createLabel(shlSlotMachine, "New Label", SWT.NONE);
-		slots[1].setBounds(225, 100, 150, 150);
+		slots[1] = formToolkit.createLabel(composite, "New Label", SWT.NONE);
+		slots[1].setBounds(205, 1, 150, 150);
 		slots[1].setImage(SWTResourceManager.getImage(SlotMachine.class, "/Immagini/uva.PNG"));
 		
-		slots[2] = formToolkit.createLabel(shlSlotMachine, "New Label", SWT.NONE);
-		slots[2].setBounds(376, 100, 150, 150);
+		slots[2] = formToolkit.createLabel(composite, "New Label", SWT.NONE);
+		slots[2].setBounds(400, 1, 150, 150);
 		slots[2].setImage(SWTResourceManager.getImage(SlotMachine.class, "/Immagini/bar.PNG"));
+
 		
 		Button btnGenera = new Button(shlSlotMachine, SWT.NONE);
 		btnGenera.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				generaSlot();
+				Gira();
 			}
 		});
 		btnGenera.setBounds(10, 282, 561, 68);
@@ -89,8 +117,13 @@ public class SlotMachine {
 		
 		titolo = new Label(shlSlotMachine, SWT.NONE);
 		titolo.setAlignment(SWT.CENTER);
-		titolo.setBounds(4, 4, 567, 68);
+		titolo.setBounds(4, 4, 567, 61);
 		titolo.setText("Best slot machine ever made by Gastaldo && Demian Oleksandr");
+	}
+	
+	private void Gira(){
+		GiraSlot g = new GiraSlot();
+		g.run();
 	}
 	
 	private void generaSlot(){
@@ -103,12 +136,12 @@ public class SlotMachine {
 	
 	private void Controlla(){
 		if(numeri[0] != numeri[1] && numeri[1] != numeri[2]){
+			titolo.setFont(SWTResourceManager.getFont("Segoe UI", 14, SWT.NORMAL));
 			titolo.setText("Hai stra perso, perdente");
-			titolo.setFont(SWTResourceManager.getFont("Segoe UI", 10, SWT.NORMAL));
 		}
 		if(numeri[0] == numeri[1] && numeri[1] == numeri[2]){
+			titolo.setFont(SWTResourceManager.getFont("Segoe UI", 20, SWT.NORMAL));
 			titolo.setText("HAI MEGA VINTO, PERDENTE");
-			titolo.setFont(SWTResourceManager.getFont("Segoe UI", 18, SWT.NORMAL));
 		}
 	}
 }
