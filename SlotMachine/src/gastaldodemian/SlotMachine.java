c379075b61be4f75d5048e05b308da19228afe8c
demianoleksandr <demianoleksandr@Lab06_21.itseinaudi.local>
2016-03-02 08:07:10 +0100

    Barre

--- SlotMachine/src/gastaldodemian/SlotMachine.java
+++ SlotMachine/src/gastaldodemian/SlotMachine.java
@@ -86,7 +86,9 @@ public class SlotMachine {
 		shlSlotMachine.setSize(600, 400);
 		shlSlotMachine.setText("Slot Machine");
 		
-		Composite composite = new Composite(shlSlotMachine, SWT.NONE);
+		Composite composite = new Composite(shlSlotMachine, SWT.BORDER);
+		composite.setForeground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_FOREGROUND));
+		composite.setBackground(SWTResourceManager.getColor(SWT.COLOR_WIDGET_FOREGROUND));
 		composite.setBounds(11, 71, 560, 152);
 		formToolkit.adapt(composite);
 		formToolkit.paintBordersFor(composite);
@@ -100,6 +102,14 @@ public class SlotMachine {
 		slots[1].setImage(SWTResourceManager.getImage(SlotMachine.class, "/Immagini/uva.PNG"));
 		
 		slots[2] = formToolkit.createLabel(composite, "New Label", SWT.NONE);
+		
+		Label label = new Label(composite, SWT.SEPARATOR | SWT.VERTICAL);
+		label.setBounds(161, 0, 1, 150);
+		formToolkit.adapt(label, true, true);
+		
+		Label label_1 = new Label(composite, SWT.SEPARATOR | SWT.VERTICAL);
+		label_1.setBounds(392, 0, 16, 148);
+		formToolkit.adapt(label_1, true, true);
 		slots[2].setBounds(400, 1, 150, 150);
 		slots[2].setImage(SWTResourceManager.getImage(SlotMachine.class, "/Immagini/bar.PNG"));
 
